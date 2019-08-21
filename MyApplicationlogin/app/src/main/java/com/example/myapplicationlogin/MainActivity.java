a1ce8a6b9347a3ef7afea1b0a5eae7014870749f
treinamento <treinamento@07HW004348.SOAM.TCS.com>
2019-08-21 16:59:59 -0300

    Login/senha

--- MyApplicationlogin/app/src/main/java/com/example/myapplicationlogin/MainActivity.java
+++ MyApplicationlogin/app/src/main/java/com/example/myapplicationlogin/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplicationlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,13 +16,24 @@ public class MainActivity extends AppCompatActivity {
     private Button start;
     private EditText login;
     private EditText pass;
+    private SharedPreferences pref;
+    private final String LOG_IN_STATUS="Login status";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref= this.getPreferences(Context.MODE_PRIVATE);
+        if (getValue(LOG_IN_STATUS)){
+            Intent intent = new Intent(this, DActivity.class);
+            startActivity(intent);
+        } else {
+
+        }
 
         setListener();
+
+
     }
 
     void setListener(){
@@ -30,7 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick (View view){
+                String user = login.getText().toString();
+                String prot = pass.getText().toString();
+                if (user!= null && pass!=null){
+                    if (user.equals("Emerson") && pass.equals("321")){
+                        Intent i = new Intent(MainActivity.this, DActivity.class);
+                        i.putExtra("Emerson", login);
+                        setValue(true);
+                        startActivity(i);
+                        finish();
+                    }
+                }
+            }
+            /*public void onClick(View view) {
                 String user = login.getText().toString();
                 String prot = pass.getText().toString();
                 if (user!= null){
@@ -42,6 +68,18 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"error", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        });*/
+    }
+    void saveValue(boolean status){
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean(LOG_IN_STATUS, status);
+        editor.commit();
+
+    }
+    boolean getValue(String key){
+        boolean defaultValue = false;
+        boolean value = pref.getBoolean(key, defaultValue);
+        return value;
+
     }
 }
